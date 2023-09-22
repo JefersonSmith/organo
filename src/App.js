3d74e3f15d7856c5c6a82d0c939659076afc11fa
Jeferson <jjsmithcrf@gmail.com>
2023-09-22 15:19:08 -0300

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Banner from './components/Banner';
+import CampoTexto from './components/CampoTexto';
+
+function App() {
+  return (
+    <div className="App">
+      <Banner />
+      <CampoTexto label=""/>
+      <CampoTexto label=""/>
+      <CampoTexto label=""/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import './style.css';
+function Banner() {
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="Banner principal da página Organo" />
+        </header>
+    )
+}
+
+export default Banner;
