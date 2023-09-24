c32d21e8b8ab99251b2200e546bf0efc757c3bfb
CRFlam <jjsmithcrf@gmail.com>
2023-09-24 00:31:59 -0300

    criando componente time e elevando lista de times

--- src/components/Formulario/index.js
+++ src/components/Formulario/index.js
@@ -2,35 +2,66 @@ import "./styles.css";
 import CampoTexto from "../CampoTexto";
 import ListaSuspensa from "../ListaSuspensa";
 import Botao from "../Botao";
+import { useState } from "react";
 
-const Formulario = () => {
-
+const Formulario = (props) => {
   const times = [
-    'Programação',
-    'Frontend',
-    'Data Science',
-    'Devops',
-    'UX e Design',
-    'Mobile',
-    'Inovação e Gestão'
-  ]
+    "Programação",
+    "Frontend",
+    "Data Science",
+    "Devops",
+    "UX e Design",
+    "Mobile",
+    "Inovação e Gestão"
+  ];
+
+  const [nome, setNome] = useState("");
+  const [cargo, setCargo] = useState("");
+  const [imagem, setImagem] = useState("");
+  const [time, setTime] = useState("");
 
   const aoSalvar = (evento) => {
-    evento.preventDefault()
-    console.log('Form foi submetido')
-  }
+    evento.preventDefault();
+    props.aoColaboradorCadastrado({
+      nome,
+      cargo,
+      imagem,
+      time
+    });
+  };
 
   return (
     <section className="formulario">
       <form onSubmit={aoSalvar}>
         <h2>Preencha os dados para criar o card do colaborador</h2>
-        <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite o seu nome" />
-        <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite o seu cargo" />
-        <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
-        <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
-        <Botao>
-          Criar Card
-        </Botao>
+        <CampoTexto
+          obrigatorio={true}
+          label="Nome"
+          placeholder="Digite o seu nome"
+          valor={nome}
+          aoAlterado={setNome}
+        />
+        <CampoTexto
+          obrigatorio={true}
+          label="Cargo"
+          placeholder="Digite o seu cargo"
+          valor={cargo}
+          aoAlterado={setCargo}
+        />
+        <CampoTexto
+          label="Imagem"
+          placeholder="Digite o endereço da imagem"
+          valor={imagem}
+          aoAlterado={setImagem}
+        />
+        <ListaSuspensa
+          obrigatorio={true} 
+          label="Time" 
+          itens={times} 
+          valor={time}
+          aoAlterado={valor => setTime(valor)}
+         />
+        <Botao>Criar Card</Botao>
       </form>
     </section>
   );
--- src/components/Time/index.js
+++ src/components/Time/index.js
@@ -1,9 +1,11 @@
 import './styles.css'
 
 const Time = (props) => {
-    return (
-        <section>
-            <h3>{props.nome}</h3>
+const css = { backgroundColor: props.corSecundaria }
+
+    return (        
+        <section className='time' style={css}>
+            <h3 style={{borderColor: props.corPrimaria}}>{props.nome}</h3>
         </section>
     )
 }
